c919f2e94b53adcd9464555a4b93ab52aa00ecd3
Sreedevi <sreedevi@luminartechnolab.com>
2022-11-21 15:14:29 +0530

    commit

---
+++ bin/variablesss2.dart
@@ -0,0 +1,15 @@
+void main(){
+  String name = "Anu";
+  int   age   = 20;
+  int phone   = 9876543210;
+  double mark = 8.3;
+  String email = "anu10@gmail.com";
+
+  print("----My Resume----");
+  print("my name is $name");  //string interpolation = ${variablename}  my name is Anu
+  print('I am $age yrs old');
+  print('My phone number is $phone');
+  print("I have $mark cgpa in my graduation");
+  print('My email id is $email');
+
+}
